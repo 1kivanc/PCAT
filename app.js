af6a5541f4336a07e0d1f11fb13d57199dd11147
1kivanc <kivanc0556@gmail.com>
2023-02-11 18:28:01 +0300

    update photo information

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./models/Photo');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const fs = require('fs');
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 const port = 3000;
 
 app.get('/', async (req, res) => {
@@ -66,6 +68,22 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req,res) => {
+    const photo = await Photo.findOne({_id:req.params.id});
+    photo.title = req.body.title
+    photo.description = req.body.description
+    photo.save()
+
+    res.redirect(`/photos/${req.params.id}`)
 });
 
 app.listen(port, () => {
